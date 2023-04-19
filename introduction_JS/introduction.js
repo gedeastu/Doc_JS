5ce31c556020a47471c0f61e4fd13cb7389a6a37
gedeastu <gede.astu@gmail.com>
2023-04-19 20:03:28 +0700

    up_training01_DONE

--- introduction_JS/introduction.js
+++ introduction_JS/introduction.js
@@ -1,5 +1,5 @@
 // ALERT
-// for pop up chat 
+// for pop up chat
 // alert(`What's UP`);
 
 // CONSOLE.LOG
@@ -10,14 +10,13 @@
 // // it will pop up but would to showed the input
 // prompt(`input your age : `)
 
-
 // VARIABLE in JS
 // type "let" is good when you wanna make a variable and you could to changes a new value on this variable
 // let age = 18;
 // console.log(`hi, you're too young ${age}`)
 
 // type "const" is good when wanna make the variable with a absolute value
-// note: "const" is doesn't changes the value if you did 
+// note: "const" is doesn't changes the value if you did
 // const yourAge = 10;
 // yourAge = 20;
 // console.log(`hi, you're too young ${yourAge}`)
@@ -25,7 +24,6 @@
 // type "var" is often used when you've learned about object JS but is doesn't really good for make a variable
 // var Age = 12;
 // console.log(`hi, you're too young ${Age}`);
-
 
 // DATA TYPE IN JS
 // String
@@ -41,33 +39,29 @@ let giveUp = null;
 
 //CONDITION LOGICAL
 //IF ELSE
-if(giveUp == null){
-    giveUp = `Tap outside alert`;
-}else{
-    giveUp = `i'am Joke!`;
+if (giveUp == null) {
+  giveUp = `Tap outside alert`;
+} else {
+  giveUp = `i'am Joke!`;
 }
 //SWITCH
-switch(version){
-    case 16.9:
-    version = 16.9; 
+switch (version) {
+  case 16.9:
+    version = 16.9;
     break;
 
-    case 17:
+  case 17:
     version = 17;
     break;
 
-    default:{
-        version = `Demo Version`;
-    }
+  default: {
+    version = `Demo Version`;
+  }
 }
-
 
 // EXAMPLE "alert" of source code above
 alert(`${desc} with type is ${type} on version ${version} until ${until}, 
 Ready? Yes = press "OK" || Ready? No = ${giveUp}`);
-
-
-
 
 // COMBINATION
 // "let" variable + alert
@@ -77,25 +71,25 @@ alert(`Welcome to the Page ${name}`);
 //"let" variable + prompt/alert with "If" condition
 let usia = prompt(`when you was born ?`);
 let operationAge = 2023 - usia;
-if(operationAge < 17){
-    alert(`you're too young`);
-}else{
-    alert(`you was ${operationAge}`);
+if (operationAge < 17) {
+  alert(`you're too young`);
+} else {
+  alert(`you was ${operationAge}`);
 }
 
 //"let" variable + prompt/alert with "switch" condition
 let inputIncome = prompt(`input your income
-Rp.`)
-let inputExpend = prompt(`input your expend Rp.`)
-const totalSaldo = inputIncome - inputExpend; 
-switch(totalSaldo){
-    case totalSaldo < 20000:
-        alert(`you have ${totalSaldo} please be thrifty`); 
+Rp.`);
+let inputExpend = prompt(`input your expend Rp.`);
+const totalSaldo = inputIncome - inputExpend;
+switch (totalSaldo) {
+  case totalSaldo < 20000:
+    alert(`you have ${totalSaldo} please be thrifty`);
     break;
-    case totalSaldo > 20000:
-        alert(`you have ${totalSaldo} keep it up!!`);
+  case totalSaldo > 20000:
+    alert(`you have ${totalSaldo} keep it up!!`);
     break;
-    default:{
-        alert(`You didn't have a saldo lately`);
-    }
+  default: {
+    alert(`You didn't have a saldo lately`);
+  }
 }
--- introduction_JS/training01.js
+++ introduction_JS/training01.js
@@ -1,8 +1,49 @@
 // TRAINING 01 key(Gunakan alert() dan prompt() pada kedua soal tersebut)
 
 // 1. prompt() untuk mengetahui saldo akhir dari apa yang diinputkan oleh user
-
+let Income = prompt(`Input your Income :
+`);
+let expends = prompt(`Input your Expend : 
+`);
+let daysUser = new Date().getDate();
+let total = Income - expends;
+let bonus;
+if(Income > 100000 && expends < 50000){
+    bonus= total + 0.20 * total;
+    alert(`Congratulations you have total ${bonus}, Keep it up!`);
+}else if(Income >= 50000 && expends <20000){
+    bonus= total + 0.10 * total;
+    alert(`Congratulations you have total ${bonus}, Come on u can more than like that!!`)
+}else{
+    alert(`it's okay, be struggle!`)
+}
 
 // 2. menentukan hari dari tanggal yang ada pada pc/laptop
 // note: getDay mengubah hari menjadi number, contoh => senin : 1, selasa : 2, dst
 let days = new Date().getDay();
+switch(days){
+    case 1:
+        alert(`today is Senin`);
+    break;
+    case 2:
+        alert(`today is Selasa`);
+    break;
+    case 3:
+        alert(`today is Rabu`);
+    break;
+    case 4:
+        alert(`today is Kamis`);
+    break;
+    case 5:
+        alert(`today is Jum'at`);
+    break;
+    case 6:
+        alert(`today is Sabtu`);
+    break;
+    case 7:
+        alert(`today is Minggu`);
+    break;
+    default:{
+        alert(`please try again`);
+    };
+};
