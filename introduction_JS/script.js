2492900f683e59fffdfdbcdba2035cf9652d29c1
gedeastu <gede.astu@gmail.com>
2023-04-19 12:51:29 +0700

    up_introduction

---
+++ introduction_JS/script.js
@@ -0,0 +1,30 @@
+// ALERT
+// for pop up chat 
+alert(`What's UP`);
+
+// CONSOLE.LOG
+// for displayed the value in console
+console.log(`Hello World`);
+
+// PROMPT
+// it will pop up but would to showed the input
+prompt(`input your age : `)
+
+
+// VARIABLE in JS
+//type "let" is good when you wanna make a variable and you could to changes a new value on this variable
+let age = 18;
+console.log(`hi, you're too young ${age}`)
+
+//type "const" is good when wanna make the variable with a absolute value
+//note: "const" is doesn't changes the value if you did 
+const yourAge = 10;
+// yourAge = 20;
+console.log(`hi, you're too young ${yourAge}`)
+
+// type "var" is often used when you've learned about object JS but is doesn't really good for make a variable
+var Age = 12;
+console.log(`hi, you're too young ${Age}`);
+
+
+// COMBINATION
