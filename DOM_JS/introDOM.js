ff6693ef385a39e4b8c752413a07561bb6c20203
gedeastu <gede.astu@gmail.com>
2023-04-20 09:52:34 +0700

    up_introDOM

---
+++ DOM_JS/introDOM.js
@@ -0,0 +1,42 @@
+// DOM for title in HTML
+console.log(document.title); //=> console.log for troubleshooting
+let titleDOM = document.title;
+titleDOM = 'Intro DOM - Javascript';
+
+
+// DOM for body in HTML
+console.log(document.body); //=> console.log for troubleshooting
+let bodyDOM = document.body;
+
+// "append()" is input the content in HTML body and showed the element from HTML
+bodyDOM.append(`WHAT'S UP`);
+
+// INPUT VALUE => innerHTML, textContent, innerText
+//1. "innerHTMl" (akan menginput atau menyimpan value pada elemen HTML dan fungsi pada elemennya akan bekerja)
+let innerHTML = document.createElement(`h1`);
+innerHTML.innerHTML = `<marquee>ini pakai innerHTML</marquee>`;
+bodyDOM.append(innerHTML);
+//2. "textContent" (akan menginput atau menyimpan value menjadi sebuah text)
+let textContent = document.createElement('h1');
+textContent.textContent = `<marquee>ini pakai textContent</marquee>`;
+bodyDOM.append(textContent);
+//3. "innerText" (akan menginput atau menyimpan value menjadi text namun dikhususkan untuk utilities text, contohnya: <span>,<strong>,<b>,dll)
+let innerText = document.createElement('i');
+innerText.innerText = `<marquee>ini pakai textContent</marquee>`;
+bodyDOM.append(innerText);
+
+// ANOTHER EXAMPLE : 
+// 1. "append()" with "createElement()"
+let intro = document.createElement(`h1`);
+intro.innerHTML = `Welcome to the Learning DOM JS`;
+bodyDOM.append(intro);
+let content = document.createElement(`p`);
+content.innerHTML = `My name is Gede Astu Nugraha`;
+bodyDOM.append(content);
+let visitLink = document.createElement(`a`);
+visitLink.href(`#`);
+visitLink.innerHTML = `about me`;
+bodyDOM.append(visitLink);
+
+
+
