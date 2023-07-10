70347090077b1fb679cb06a71495946998ccfbb5
gedeastu <gede.astu@gmail.com>
2023-07-10 18:51:54 +0700

    up

--- DOM_JS/DOM_selection/DOMSelection.js
+++ DOM_JS/DOM_selection/DOMSelection.js
@@ -1,1 +1,21 @@
+// DOM Selection Method :
+// getElementById() -> element
+const Judul = document.getElementById("judul");
+Judul.style.color = 'white';
+Judul.style.backgroundColor = '#F7A300';
+Judul.innerHTML = 'Hello My User! 👋';
 
+// getElementsByTagName() -> HTML Collection
+const p = document.getElementsByTagName("p");
+for (let i = 0; i <= p.length; i++){
+    if(i % 2 == 0){
+        p[i].style.backgroundColor = '#378F17';
+        p[i].style.color = "white";
+        console.log(i);
+    }
+}
+
+
+// getElementsByClassName() -> HTML Collection
+// querySelector() -> element
+// querySelectorAll() -> nodeList
